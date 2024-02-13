69e9857421a594883425deefad79de0c119bc31a
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2024-02-13 17:37:52 +0200

    with method split

--- hw_9_1.py
+++ hw_9_1.py
@@ -1,16 +1,14 @@
 def popular_words(text, words):
-    numbers_ = []
+    num_ = []
     my_dict = {}
 
-    for i in range(len(words)):
-        quantity = text.lower().count(' ' + words[i] + ' ')
-        if quantity > 0:
-            numbers_.append(quantity)
-        else:
-            numbers_.append(0)
+    text_low = text.lower().split()
 
     for i in range(len(words)):
-        my_dict.update({words[i]: numbers_[i]})
+        num_.append(text_low.count(words[i]))
+
+    for i in range(len(words)):
+        my_dict.update({words[i]: num_[i]})
 
     return my_dict
 
